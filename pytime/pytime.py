e5469de088c6549fe8acbba2bb4b4dee94c900b2
Ciaran Soden <ciaran.soden@hotmail.co.uk>
2023-12-07 20:21:37 +0000

    V0.1
    Basic timing, progress bar, overall result creation

---
+++ pytime/pytime.py
@@ -0,0 +1,27 @@
+import time as t
+import os
+
+
+class pytime:
+    def calcAverageExec(self, func, args, its=0):
+        tot = 0
+        for i in range(its):
+            if (i % (its/100)) == 0:
+                self.printProgress(int(i//(its/100)))
+            a = t.time()
+            func(*args)
+            b = t.time()
+            tot += (b-a)
+        os.system("cls")
+        self.getResult(tot, its)
+        
+    def printProgress(self, i):
+        os.system("cls")
+        print("-"*102)
+        print("|" + "/"*i + " "*(100-i) + "|" + f" {i}%")
+        print("-"*102)
+        
+    def getResult(self, tot, its):
+        print("-"*102)
+        print(" "*20 + "Total iterations " + "|" + f"{its}" + "|" + " "*10 + "Average time taken " + "|" + f"{round((1000*tot/its), 5)}" + "ms|")
+        print("-"*102)
---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+set(name="pytime",
+    version="0.1",
+    description="Rudamentary way of calculating the average time take to execute a funciton in Python",
+    packages=["pytime.py"],
+    zip_safe=False)
